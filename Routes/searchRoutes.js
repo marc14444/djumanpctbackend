d1432cc87bd1536cbd1723cfbc6721c88a0db3ec
marc14444 <amanijeanmarc41@gmail.com>
2024-12-17 12:13:51 +0000

    gestion de la routes des search

--- Routes/searchRoutes.js
+++ Routes/searchRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { searchArtisans } from "../controllers/searchController.js";
+import authArtisan from "../middleware/authArtisan.js";
+import { searchArtisans } from "../Controllers/searchController.js";
 
 const router = express.Router();
 
 // Route pour rechercher les artisans
-router.get("/get-artisans-by-search", searchArtisans);
+router.get("/get-artisans-by-search",authArtisan, searchArtisans);
+
+//exemple de requête : http://localhost:3000/search?latitude=48.8566140&longitude=2.3522219&metier=tailleur&rayon=10&mode=voiture
 
 export default router;
